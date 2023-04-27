abda9077fdb3580091737d02247e29c8e1648814
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-04-27 12:52:18 +0545

    first commit

---
+++ inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class A
+{
+    public void talk()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
+public class B : A
+{
+    public void say()
+    {
+        Console.WriteLine("My name is Aanchal");
+    }
+}
+public class c : A
+{
+    public void speak()
+    {
+        Console.WriteLine("My name is Reshma");
+    }
+}
+class Myclass
+{
+    static void Main()
+    {
+        B myB = new B();
+        myB.talk();
+        myB.say();
+
+        c myc = new c();
+        myc.talk();
+        myc.speak();
+    }
+}
